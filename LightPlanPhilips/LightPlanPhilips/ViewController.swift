24de9c1200f9cb35ba955cfe46eb740d93d8d597
Erik Waterham <waterham@gmail.com>
2016-11-07 15:30:54 +0100

    - Eerste versie

---
+++ LightPlanPhilips/LightPlanPhilips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightPlanPhilips
+//
+//  Created by Erik Waterham on 07/11/2016.
+//  Copyright © 2016 The App Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
