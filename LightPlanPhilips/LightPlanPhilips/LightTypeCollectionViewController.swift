e0052679f1adc0276a2fb4cb2876677248978200
Erik Waterham <waterham@gmail.com>
2016-11-11 15:54:18 +0100

    LightTypeCollectionViewController met koppeling naar LightType zonder informatie in cell (zwart)

---
+++ LightPlanPhilips/LightPlanPhilips/LightTypeCollectionViewController.swift
@@ -0,0 +1,98 @@
+//
+//  LightTypeCollectionViewController.swift
+//  LightPlanPhilips
+//
+//  Created by Erik Waterham on 11/11/2016.
+//  Copyright © 2016 The App Academy. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "lightTypeCell"
+private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+
+class LightTypeCollectionViewController: UICollectionViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Register cell classes
+    self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using [segue destinationViewController].
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+  // MARK: UICollectionViewDataSource
+  
+  override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 1
+  }
+  
+  
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of items
+    return  DataLightPlan.sharedInstance.listLightType.count
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    cell.backgroundColor = UIColor.black
+    
+    // Configure the cell
+    
+    return cell
+  }
+  
+  // MARK: UICollectionViewDelegate
+  
+  /*
+   // Uncomment this method to specify if the specified item should be highlighted during tracking
+   override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+   return true
+   }
+   */
+  
+  /*
+   // Uncomment this method to specify if the specified item should be selected
+   override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+   return true
+   }
+   */
+  
+  /*
+   // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+   override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+   return false
+   }
+   
+   override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+   return false
+   }
+   
+   override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+   
+   }
+   */
+  
+}
+
+//extension LightTypeCollectionViewController:
