6fad5d0eb0a7ee2aa51e168d5913e3e122012956
Erik Waterham <waterham@gmail.com>
2016-11-14 17:29:11 +0100

    Nu met nieuwe lamptypes

--- LightPlanPhilips/LightPlanPhilips/DataLightPlan.swift
+++ LightPlanPhilips/LightPlanPhilips/DataLightPlan.swift
@@ -9,23 +9,31 @@
 import UIKit
 
 class DataLightPlan: NSObject {
-    static let  sharedInstance = DataLightPlan()
-
-    var listLightType = [
-        LightType(name: "Dining",     file: "Dining.png"),
-        LightType(name: "Floorstand", file: "Floorstand.png"),
-        LightType(name: "Pendant",    file: "Pendant.png")]
+  static let  sharedInstance = DataLightPlan()
+  
+  var listLightType = [
+    LightType(name: "br30 slim",               file: "br30_slim.png"),
+    LightType(name: "ceiling",                 file: "ceiling.png"),
+    LightType(name: "floor",                   file: "floor.png"),
+    LightType(name: "go",                      file: "go.png"),
+    LightType(name: "gu10 perfectfit",         file: "gu10_perfectfit.png"),
+    LightType(name: "lichtstrip",              file: "lightstrip.png"),
+    LightType(name: "pendant",                 file: "pendant.png"),
+    LightType(name: "rec spot",                file: "rec_spot.png"),
+    LightType(name: "spot",                    file: "spot.png"),
+    LightType(name: "table",                   file: "table.png"),
+    LightType(name: "white and color e27 b22", file: "white_and_color_e27_b22.png")]
+  
+  var listLamp = [
+    Lamp(name: "Bulb 1"),
+    Lamp(name: "Bulb 2"),
+    Lamp(name: "Bulb 3"),
+    Lamp(name: "Bulb 4"),
+    Lamp(name: "Bulb 5"),
+    Lamp(name: "Bulb 6")]
+  
+  func loadLightPlan() {
     
-    var listLamp = [
-        Lamp(name: "Bulb 1"),
-        Lamp(name: "Bulb 2"),
-        Lamp(name: "Bulb 3"),
-        Lamp(name: "Bulb 4"),
-        Lamp(name: "Bulb 5"),
-        Lamp(name: "Bulb 6")]
-    
-    func loadLightPlan() {
-        
-    }
-
+  }
+  
 }
