de0ed512ece5a8783b6d075ff1cd364fca035c95
Erik Waterham <waterham@gmail.com>
2016-11-18 09:16:00 +0100

    Uitwerken RoomShapeViewController

--- LightPlanPhilips/LightPlanPhilips/RoomShapeViewController.swift
+++ LightPlanPhilips/LightPlanPhilips/RoomShapeViewController.swift
@@ -28,6 +28,69 @@ class RoomShapeViewController: UIViewController {
   var cellW = false
   var cellNW = false
   
+  enum Operation {
+    case Corner((Int, Angle) -> Bool)
+    case Side((Int, Angle) -> Bool)
+    case Inner((Int) -> Bool)
+  }
+  
+  enum Angle {
+    case Normal
+    case Right
+    case Half
+    case Left
+  }
+  //  func performOperation(index: Int) {
+  //    if let operation = operations[index] {
+  //      switch operation {
+  //      case
+  //    }
+  //  }
+  
+//  var operations = [ Operation.Corner(allowedCorner(index: 0, Angle.Normal)),
+//                     Operation.Side(allowedSide( index: 1, Angle.Right)),
+//                     Operation.Side(allowedSide( index: 2, Angle.Right)),
+//                     Operation.Side(allowedSide( index: 3, Angle.Right)),
+//                     Operation.Side(allowedSide( index: 4, Angle.Right)),
+//                     Operation.Side(allowedSide( index: 5, Angle.Right)),
+//                     Operation.Corner(allowedCorner(index: 6, Angle.Right)),
+//                     Operation.Side(allowedSide(index: 7, Angle.Normal)),
+//                     Operation.Inner(allowedInner(index: 8)),
+//                     Operation.Inner(allowedInner(index: 9)),
+//                     Operation.Inner(allowedInner(index: 10)),
+//    Operation.Inner(allowedInner(index: 11)),
+//    Operation.Inner(allowedInner(index: 12)),
+//    Operation.Side(allowedSide(index: 13, Angle.Half)),
+//    Operation.Inner(allowedInner(index: 13))
+//    
+//  ]
+  
+  
+  func allowedCorner( index: Int, angle: Angle) -> Bool {
+    return true
+  }
+  
+  func allowedSide( index: Int, angle: Angle) -> Bool {
+    return true
+  }
+  
+  func allowedInner( index: Int) -> Bool {
+    return true
+  }
+  
+  @IBAction func cellButton(_ sender: Any) {
+    let cellButton = sender as! UIButton
+    let cellIndex = Int(cellButton.currentTitle!)!
+    if toggle[cellIndex] {
+      cellButton.backgroundColor = UIColorFromRGB(rgbValue: 0xD8D8D8)
+      cellButton.setTitleColor(UIColorFromRGB(rgbValue: 0xD8D8D8), for: .normal)
+    } else {
+      cellButton.backgroundColor = UIColorFromRGB(rgbValue: 0xE7BF7E)
+      cellButton.setTitleColor(UIColorFromRGB(rgbValue: 0xE7BF7E), for: .normal)
+    }
+    toggle[cellIndex] = !toggle[cellIndex]
+    
+  }
   struct Point {
     var x: Int
     var y: Int
@@ -36,19 +99,23 @@ class RoomShapeViewController: UIViewController {
       y = 0
     }
   }
-//  let index2Point = [ Point(0,0), Point(1,0)]
-
+  //  let index2Point = [ Point(0,0), Point(1,0)]
+  
   struct Cell {
     var Point = (0, 0)
     var state = false
     
   }
   
+  var toggle = Array(repeating: Bool(), count: 49)
+  
+  
   @IBAction func cancelButton(_ sender: Any) {
     dismiss(animated: true, completion: nil)
   }
   override func viewDidLoad() {
     super.viewDidLoad()
+    toggle = toggle.map{ _ in false}
     
     // Do any additional setup after loading the view.
   }
@@ -56,6 +123,15 @@ class RoomShapeViewController: UIViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+    return UIColor(
+      red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+      green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+      blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+      alpha: CGFloat(1.0)
+    )
   }
   
   
