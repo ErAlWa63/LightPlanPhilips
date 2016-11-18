034d0d2700463a064bd6915a7aeaa42030ff07a6
Erik Waterham <waterham@gmail.com>
2016-11-18 15:37:40 +0100

    Collection View aanpassingen in D1.2

--- LightPlanPhilips/LightPlanPhilips/CreateLightGroupBulbCollectionView.swift
+++ LightPlanPhilips/LightPlanPhilips/CreateLightGroupBulbCollectionView.swift
@@ -8,13 +8,24 @@
 // http://stackoverflow.com/questions/31582378/ios-8-swift-tableview-with-embedded-collectionview
 //
 // http://stackoverflow.com/questions/17856055/creating-a-uicollectionview-programmatically
+//
+// http://stackoverflow.com/questions/35856630/embedded-uicollectionview-in-view-controller-not-getting-called-swift
 
 import UIKit
 
 class CreateLightGroupBulbCollectionView: UICollectionView {
-  
+  let d = D() // debugger functionality
+
+  fileprivate let reuseIdentifier = "lightGroupTypeCell"
+  fileprivate let sectionInsets = UIEdgeInsets(top: 18.0, left: 10.0, bottom: 18.0, right: 10.0)
+  fileprivate let itemsPerRow: CGFloat = 2
+
   var collectionView: UICollectionView!
   
+//  override init(
+  
+//  createLightGroupBulbCollectionView.delegate = self
+//  createLightGroupBulbCollectionView.dataSource = self
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -105,9 +116,9 @@ class CreateLightGroupBulbCollectionView: UICollectionView {
 //  open var numberOfSections: Int { get }
 //  
 //  
-  open override func numberOfItems(inSection section: Int) -> Int {
-    return 2
-  }
+//  open override func numberOfItems(inSection section: Int) -> Int {
+//    return 2
+//  }
 //
 //  
 //  open func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes?
@@ -185,31 +196,148 @@ class CreateLightGroupBulbCollectionView: UICollectionView {
 //
 }
 
-extension CreateLightGroupViewController: UICollectionViewDataSource {
-  public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 2
-  }
+extension CreateLightGroupBulbCollectionView: UICollectionViewDelegate {
+  //
+  //
+  //  // Methods for notification of selection/deselection and highlight/unhighlight events.
+  //  // The sequence of calls leading to selection from a user touch is:
+  //  //
+  //  // (when the touch begins)
+  //  // 1. -collectionView:shouldHighlightItemAtIndexPath:
+  //  // 2. -collectionView:didHighlightItemAtIndexPath:
+  //  //
+  //  // (when the touch lifts)
+  //  // 3. -collectionView:shouldSelectItemAtIndexPath: or -collectionView:shouldDeselectItemAtIndexPath:
+  //  // 4. -collectionView:didSelectItemAtIndexPath: or -collectionView:didDeselectItemAtIndexPath:
+  //  // 5. -collectionView:didUnhighlightItemAtIndexPath:
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath)
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath)
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool // called when the user taps on an already-selected item in multi-select mode
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath)
+  //
+  //
+  //  @available(iOS 8.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath)
+  //
+  //  @available(iOS 8.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath)
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath)
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, didEndDisplayingSupplementaryView view: UICollectionReusableView, forElementOfKind elementKind: String, at indexPath: IndexPath)
+  //
+  //
+  //  // These methods provide support for copy/paste actions on cells.
+  //  // All three should be implemented if any are.
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool
+  //
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?)
+  //
+  //
+  //  // support for custom transition layout
+  //  @available(iOS 7.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, transitionLayoutForOldLayout fromLayout: UICollectionViewLayout, newLayout toLayout: UICollectionViewLayout) -> UICollectionViewTransitionLayout
+  //
+  //
+  //  // Focus
+  //  @available(iOS 9.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, canFocusItemAt indexPath: IndexPath) -> Bool
+  //
+  //  @available(iOS 9.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, shouldUpdateFocusIn context: UICollectionViewFocusUpdateContext) -> Bool
+  //
+  //  @available(iOS 9.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, didUpdateFocusIn context: UICollectionViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator)
+  //
+  //  @available(iOS 9.0, *)
+  //  optional public func indexPathForPreferredFocusedView(in collectionView: UICollectionView) -> IndexPath?
+  //
+  //
+  //  @available(iOS 9.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, targetIndexPathForMoveFromItemAt originalIndexPath: IndexPath, toProposedIndexPath proposedIndexPath: IndexPath) -> IndexPath
+  //
+  //
+  //  @available(iOS 9.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, targetContentOffsetForProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint // customize the content offset to be applied during transition or update animations
+  
+}
 
-//  // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
-//  @available(iOS 6.0, *)
-  public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    return UICollectionViewCell()
+extension CreateLightGroupBulbCollectionView: UICollectionViewDataSource {
+  public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    d.c(m: "start", f: #file, fu: #function, l: #line)
+    return 4
   }
-//
-//  
-//  @available(iOS 6.0, *)
-//  optional public func numberOfSections(in collectionView: UICollectionView) -> Int
-//  
-//  
-//  // The view that is returned must be retrieved from a call to -dequeueReusableSupplementaryViewOfKind:withReuseIdentifier:forIndexPath:
-//  @available(iOS 6.0, *)
-//  optional public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView
-//  
-//  
-//  @available(iOS 9.0, *)
-//  optional public func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool
-//  
-//  @available(iOS 9.0, *)
-//  optional public func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath)
-//
+  
+  //  // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+  //  @available(iOS 6.0, *)
+  public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    d.c(m: "start", f: #file, fu: #function, l: #line)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+//    let cell: UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as! UICollectionViewCell
+    if indexPath.row%2 == 0 {
+      cell.backgroundColor = UIColor.red
+    } else {
+      cell.backgroundColor = UIColor.yellow
+    }
+    
+    return cell
+//    let myImageView   = UIImageView(frame: CGRect(x: (cell.frame.width / 2) - 10, y: 5, width: 20, height: 20))
+//    myImageView.image = DataLightPlan.sharedInstance.listLightType[indexPath.item].pictogram
+//    let rowString     = DataLightPlan.sharedInstance.listLightType[indexPath.item].name
+//    let myLabel       = UILabel(frame: CGRect(x: 5, y: 30, width: cell.frame.width - 10, height: 20))
+//    myLabel.font      = UIFont(name: "Apple SD Gothic Neo Regular", size: 23.0)
+//    myLabel.text      = rowString
+//    myLabel.textAlignment = .center
+//    cell.backgroundColor = UIColor.white
+//    cell.addSubview(myLabel)
+//    cell.addSubview(myImageView)
+////    if let delegateLightTypeIndex = delegateLightTypeIndex {
+////      if indexPath.row == delegateLightTypeIndex {
+////        cell.layer.cornerRadius = 5
+////        cell.layer.borderWidth = 2.0
+////        cell.layer.borderColor = UIColorFromRGB(rgbValue: 0xD21D0E).cgColor
+////      }
+////    }
+//    return cell
+//    return UICollectionViewCell()
+  }
+  
+  //  @available(iOS 6.0, *)
+  //  optional public func numberOfSections(in collectionView: UICollectionView) -> Int
+  //
+  //
+  //  // The view that is returned must be retrieved from a call to -dequeueReusableSupplementaryViewOfKind:withReuseIdentifier:forIndexPath:
+  //  @available(iOS 6.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView
+  //  
+  //  
+  //  @available(iOS 9.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool
+  //  
+  //  @available(iOS 9.0, *)
+  //  optional public func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath)
+  
 }
--- LightPlanPhilips/LightPlanPhilips/CreateLightGroupViewController.swift
+++ LightPlanPhilips/LightPlanPhilips/CreateLightGroupViewController.swift
@@ -6,27 +6,37 @@
 //  Copyright © 2016 The App Academy. All rights reserved.
 //
 // https://makeapppie.com/2016/10/10/using-tableviews-in-subviews/
+//
+// http://blog.evizija.si/ios-swift-custom-horizontal-collection-view-with-images/
 
 import UIKit
 
+var stringArray: [String] = ["1", "2", "3", "4","5", "6", "7", "8",
+                             "9", "10", "11"]
+
 class CreateLightGroupViewController: UIViewController {
-  var delegateLightGroupTypeIndex: Int?
-  var closureToPerform: ((Int) -> Void)?
-  fileprivate let reuseIdentifier = "lightGroupTypeCell"
-  fileprivate let sectionInsets = UIEdgeInsets(top: 18.0, left: 10.0, bottom: 18.0, right: 10.0)
-  fileprivate let itemsPerRow: CGFloat = 2
+  @IBOutlet weak var createLightGroupBulbCollectionView: UICollectionView!
+  
+  var selectedCell: Int! = 0
+
+//  var delegateLightGroupTypeIndex: Int?
+//  var closureToPerform: ((Int) -> Void)?
 
   @IBAction func cancelButton(_ sender: Any) {
     dismiss(animated: true, completion: nil)
  }
+//  @IBOutlet weak var createLightGroupBulbCollectionView: UICollectionView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
 //    self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 //    self.collectionView!.allowsMultipleSelection = false
 //    self.collectionView!.backgroundColor = UIColor.lightGray
     
     // Do any additional setup after loading the view.
+    selectedCell = 0
+
   }
   
   override func didReceiveMemoryWarning() {
@@ -45,4 +55,42 @@ class CreateLightGroupViewController: UIViewController {
    }
    */
   
+}
+
+//: UICollectionViewDataSource
+extension CreateLightGroupBulbCollectionView {
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return stringArray.count
+  }
+  
+  func collectionView(collectionView: UICollectionView,
+                      cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+  {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath as IndexPath)
+    let myLabel       = UILabel(frame: CGRect(x: 5, y: 30, width: cell.frame.width - 10, height: 20))
+    myLabel.font      = UIFont(name: "Apple SD Gothic Neo Regular", size: 23.0)
+    myLabel.text      = stringArray[indexPath.item]
+    myLabel.textAlignment = .center
+    cell.backgroundColor = UIColor.white
+    cell.addSubview(myLabel)
+
+//    cell.label.text = stringArray[indexPath.item]
+//    
+//    cell.layer.shouldRasterize = true;
+//    cell.layer.rasterizationScale = UIScreen.mainScreen().scale
+//    
+//    if(selectedCell != nil){
+//      if(indexPath.item == selectedCell){
+//        cell.image.image = UIImage(named: "FullCircle")!
+//        cell.label.textColor = UIColor.whiteColor()
+//      }
+//      else{
+//        cell.image.image = UIImage(named: "EmptyCircle")!
+//        cell.label.textColor = UIColor.blackColor()
+//      }
+//    }
+    
+    return cell
+  }
+
 }
