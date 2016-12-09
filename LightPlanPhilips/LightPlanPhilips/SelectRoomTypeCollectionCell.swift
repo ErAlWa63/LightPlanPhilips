9e7638c19065f9470854fee2519a549808e1220b
Erik Waterham <waterham@gmail.com>
2016-12-09 10:08:02 +0100

    DescriptionCell ontbrak nog Layout van room in tableview en collection view optimaal afgeregeld.

--- LightPlanPhilips/LightPlanPhilips/SelectRoomTypeCollectionCell.swift
+++ LightPlanPhilips/LightPlanPhilips/SelectRoomTypeCollectionCell.swift
@@ -3,4 +3,5 @@ import UIKit
 class PlaceFurnitureCollectionViewCell: UICollectionViewCell {
   @IBOutlet weak var pictogramCell: UIImageView!
   @IBOutlet weak var nameCell: UILabel!
+  @IBOutlet weak var descriptionCell: UILabel!
   }
